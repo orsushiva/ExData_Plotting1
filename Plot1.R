a55cdfc2b760cf370fa1cf9f55897d5beeb78bbe
Rachel Francis <42592724+rfrancis122@users.noreply.github.com>
2018-09-12 09:22:34 -0400

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,18 @@
+library("data.table")
+
+##Read in data file and select data for specified dates
+HouseholdPower <- data.table::fread(input = "household_power_consumption.txt", na.strings = "?")
+House_A <- rbind(HouseholdPower[HouseholdPower$Date=="1/2/2007",], HouseholdPower[HouseholdPower$Date=="2/2/2007",])
+
+##Transform Date to date format and create a new variable joining time & date
+House_A$Date <- as.Date(House_A$Date, format ="%d/%m/%Y")
+House_A$Time <- format(House_A$Time, format="%H:%M:%S")
+House_A <- cbind(House_A, "DateTime" = as.POSIXct(paste(House_A$Date, House_A$Time)))
+
+##Plot 1
+hist(as.numeric(House_A$Global_active_power), col = "Red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)", ylab = "Frequency")
+
+##Print PNG
+png("plot1.png", width = 480, height = 480)
+hist(as.numeric(House_A$Global_active_power), col = "Red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)", ylab = "Frequency")
+dev.off
